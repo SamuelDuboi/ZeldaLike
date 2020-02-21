15365fa1410674bdbbf8bb2a247f5c7542f3b885
Job Corentin <job.corentin@outlook.fr>
2020-02-21 18:44:29 +0100

    Robot de combat WIP reste la slash attack

--- ZeldaLikeUnity/Assets/Scripts/Ennemy/CJ_CombatRobot.cs
+++ ZeldaLikeUnity/Assets/Scripts/Ennemy/CJ_CombatRobot.cs
@@ -7,11 +7,15 @@ namespace Ennemy
   public class CJ_CombatRobot : SD_EnnemyGlobalBehavior
   {
         [Range(0, 20)]
-        public float maxChargeRange;
+        public float chargeRange;
         [Range(0,20)]
-        public float minChargeRange;
+        public float slashRange;
+        [Range(0,5)]
+        public float stopRange;
         [Range(0, 20)]
         public float chargeSpeed;
+        [Range(0, 20)]
+        public float followSpeed;
         bool canCharge = true;
     public override void Start()
     {
@@ -21,46 +25,53 @@ namespace Ennemy
      public override void FixedUpdate()
     {
             base.FixedUpdate();
-            if(canMove == false)
-            ennemyRGB.velocity = Vector2.zero;
     }
 
         public override void Mouvement()
         {
-            if (Vector2.Distance(transform.position, player.transform.position) > maxChargeRange && canCharge == false)
+          if(Vector2.Distance(transform.position,player.transform.position) > chargeRange)
             {
-                transform.position = Vector3.MoveTowards(transform.position, player.transform.position, Time.deltaTime * speed);
+                transform.position = Vector3.MoveTowards(transform.position, player.transform.position, Time.deltaTime * followSpeed);
             }
-            else if(Vector2.Distance(transform.position,player.transform.position) > minChargeRange && Vector2.Distance(transform.position, player.transform.position) < maxChargeRange && canCharge == true)
+          else if(Vector2.Distance(transform.position, player.transform.position) <= chargeRange && Vector2.Distance(transform.position, player.transform.position) > slashRange)
             {
-                StartCoroutine(Charge());
+                if(canCharge == true)
+                {
+                    StartCoroutine(Charge());
+                }
+                else if(canCharge == false && isAggro == true)
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, player.transform.position, Time.deltaTime * speed);
+                }
             }
-            else if (Vector2.Distance(transform.position, player.transform.position) < minChargeRange && canCharge == true)
+          else if(Vector2.Distance(transform.position, player.transform.position) <= slashRange)
             {
-                StartCoroutine(SlashAttack());
-            }
-            else if (Vector2.Distance(transform.position, player.transform.position) < minChargeRange && canCharge == false)
-            {
-                transform.position = Vector3.MoveTowards(transform.position, player.transform.position, Time.deltaTime * speed);
+                if(canCharge == true)
+                {
+                    StartCoroutine(SlashAttack());
+                }
+                else
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, player.transform.position, Time.deltaTime * followSpeed);
+                }
             }
         }
 
         public IEnumerator Charge()
         {
-            LayerMask playerlayer = LayerMask.GetMask("Player");
-            RaycastHit2D charge = Physics2D.Raycast(transform.position, new Vector2(player.transform.position.x - transform.position.x, player.transform.position.y - transform.position.y),Mathf.Infinity, playerlayer);
-            
+            LayerMask playerLayer = LayerMask.GetMask("Player");
+            RaycastHit2D chargeRay = Physics2D.Raycast(transform.position, new Vector2(player.transform.position.x - transform.position.x, player.transform.position.y - transform.position.y),Mathf.Infinity, playerLayer);
+
+            canCharge = false;
+            isAggro = false;
             GetComponent<SpriteRenderer>().color = Color.red;
             yield return new WaitForSeconds(0.5f);
             GetComponent<SpriteRenderer>().color = Color.white;
-            canCharge = false;
-            ennemyRGB.velocity = new Vector2(charge.point.x - transform.position.x, charge.point.y - transform.position.y).normalized * chargeSpeed;
+            ennemyRGB.velocity = new Vector2(chargeRay.point.x - transform.position.x, chargeRay.point.y -transform.position.y).normalized * chargeSpeed;
             yield return new WaitForSeconds(0.5f);
             ennemyRGB.velocity = Vector2.zero;
-            canMove = false;
             yield return new WaitForSeconds(2f);
-            canMove = true;
-            yield return new WaitForSeconds(1f);
+            isAggro = true;
             canCharge = true;
         }
 
