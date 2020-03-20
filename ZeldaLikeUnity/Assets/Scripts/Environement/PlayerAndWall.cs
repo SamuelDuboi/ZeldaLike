7407ed09e9abe4dcfd5c5d857c86c33a1c16bb3e
Dubois <samuel.m.dubois@gmail.com>
2020-03-20 17:47:22 +0100

    création des platformes destructibles

---
+++ ZeldaLikeUnity/Assets/Scripts/Environement/PlayerAndWall.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Player;
+
+
+
+ public class PlayerAndWall : MonoBehaviour
+ {
+    int layer;
+    CompositeCollider2D WallCollider;
+
+    private void Start()
+    {
+        WallCollider = GetComponent<CompositeCollider2D>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            ContactPoint2D contact = collision.contacts[0];
+            if (Vector3.Dot(contact.normal, Vector3.down) > 0.5)
+            {
+                SD_PlayerMovement.Instance.cantDash = true;
+                SD_PlayerMovement.Instance.cantMove = true;
+                WallCollider.isTrigger = true;
+                collision.gameObject.GetComponentInParent<Rigidbody2D>().gravityScale = 10;
+                
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            layer++;
+            if (layer == 1)
+            {
+                SD_PlayerMovement.Instance.cantDash = false;
+                SD_PlayerMovement.Instance.cantMove = false;
+                WallCollider.isTrigger = false;
+                collision.gameObject.GetComponentInParent<Rigidbody2D>().gravityScale = 0;
+                layer = 0;
+            }
+        }
+    }
+}
---
+++ ZeldaLikeUnity/Assets/Scripts/Environement/SD_DestructiblePlatform.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class SD_DestructiblePlatform : MonoBehaviour
+{
+
+    Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    void OnTriggerEnter2D( Collider2D collision)
+    {
+        if (collision.gameObject.layer == 10 || collision.gameObject.layer == 12)
+            StartCoroutine(Destruction());
+    }
+
+    IEnumerator Destruction()
+    {
+        animator.SetTrigger("Fall");
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorClipInfo(0).Length);
+        tag = "Hole";
+    }
+}
