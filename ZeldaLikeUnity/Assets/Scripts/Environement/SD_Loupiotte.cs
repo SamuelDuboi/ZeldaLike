2b1bd75129726a9c43feb2f401d9d7590e60087d
Dubois <samuel.m.dubois@gmail.com>
2020-04-16 09:47:43 +0200

    Rework des loupiottes/trigger

--- ZeldaLikeUnity/Assets/Scripts/Environement/SD_Loupiotte.cs
+++ ZeldaLikeUnity/Assets/Scripts/Environement/SD_Loupiotte.cs
@@ -21,15 +21,23 @@ public class SD_Loupiotte : MonoBehaviour
     public List<GameObject> loupiottes = new List<GameObject>();
  [HideInInspector]  public int loupiottesCount;
 
+
+    Animator doorAnimator;
+    public bool Open;
     private void Start()
     {
         loupiottesCount = 0;
+       
+        doorAnimator = GetComponent<Animator>();
+        doorAnimator.SetBool("Oppen", Open);
+        timer = 1;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.layer == 14 && !closeDoor && !OpenDoor && loupiottesCount == loupiottes.Count - 1)
+        if (collision.gameObject.layer == 14 && !closeDoor  && loupiottesCount == loupiottes.Count - 1)
         {
             Time.timeScale = 0;
+            moveBack = false;
             SD_PlayerMovement.Instance.cantDash = true;
             SD_PlayerMovement.Instance.cantMove = true;
             SD_PlayerAttack.Instance.cantAttack = true;
@@ -39,198 +47,107 @@ public class SD_Loupiotte : MonoBehaviour
             camera.transform.position = new Vector3(SD_PlayerMovement.Instance.transform.position.x, SD_PlayerMovement.Instance.transform.position.y, -10);
             move = true;
             cpt = 0;
+            timer = 1;
+            closeDoor = true;
         }
 
     }
+      
 
+    void Activation()
+    {
+        doorAnimator.SetTrigger("Activated");
+        doorAnimator.speed = 10f;
+    }
+    IEnumerator WaitToComeBack()
+    {
+        timer = 1;
+        yield return new WaitForSeconds(timeToReach);
+        doorAnimator.SetTrigger("ComeBack");
+
+
+    }
     private void Update()
     {
         if (move)
             camera.transform.position = Vector3.MoveTowards(camera.transform.position,
-                                                            new Vector3( transform.GetChild(0).position.x,transform.GetChild(0).position.y,-10),
+                                                            new Vector3(transform.GetChild(0).position.x, transform.GetChild(0).position.y, -10),
                                                             0.05f);
-        if (Mathf.Abs( camera.transform.position.x - transform.GetChild(0).position.x) <= 0.1f)
+        if (timer == 0)
         {
+            StartCoroutine(WaitToComeBack());
+
+        }
+        if (Mathf.Abs(camera.transform.position.x - transform.GetChild(0).position.x) < 0.1f && cpt == 0)
+        {
+            Time.timeScale = 0.1f;
             move = false;
-            if(transform.GetChild(0).localScale.x >= 0.1f)
-            {
-                OpenDoor = true;
-               
-            }
-            else
-            {
-                closeDoor = true;
-            }
-            if (transform.childCount > 1)
-            {
-                if (transform.GetChild(1).localScale.x >= 0.1f)
-                {
-                    OpenDoor2 = true;
-
-                }
-                else
-                {
-                    closeDoor2 = true;
-                }
-            }
-
-
-        }
-        if (OpenDoor)
-        {
-            if (cpt == 0)
-                timer = timeToReach;
-            timer += 0.01f;
-            if (timer > timeToReach)
-            {
-                if (transform.GetChild(0).localScale.x >= 0.1f)
-                {
-                    transform.GetChild(0).position -= new Vector3(0.02f, 0, 0);
-                    transform.GetChild(0).localScale -= new Vector3(0.01f, 0, 0);
-
-                }              
-                else
-                {
-                    if (cpt == 0)
-                    {
-                        moveBack = true;
-                        timer = 0;
-                        closeDoor = true;
-                        cpt++;
-                    }
-                    else
-                    {
-                        if (willBeBack)
-                        {
-                            loupiottes[loupiottesCount].GetComponent<SD_LoupiotteActivated>().activated = false;
-                            loupiottes[loupiottesCount].GetComponent<SpriteRenderer>().color = Color.white;
-                            loupiottes[0].GetComponent<SD_LoupiotteActivated>().activated = true;
-                            loupiottes[0].GetComponent<SpriteRenderer>().color = Color.blue;
-                        }
-                    }
-                    if(willBeBack)
-                    OpenDoor = false;
-
-                }
-                if (transform.GetChild(0).localScale.x >= 0.0909f && !OpenDoor || transform.GetChild(0).localScale.x <= 0.101f && !OpenDoor)                
-                    transform.GetChild(0).localScale = new Vector3(0.05f, 1, 0);                
-            }
-        }
-        if (closeDoor)
-        {
-            if (cpt == 0)
-                timer = timeToReach;
-            timer += 0.01f;
-            if (timer > timeToReach)
-            {
-                if (transform.GetChild(0).localScale.x <= 1.1f)
-                {
-                    transform.GetChild(0).position += new Vector3(0.02f, 0, 0);
-                    transform.GetChild(0).localScale += new Vector3(0.01f, 0, 0);
-
-                }
-                else
-                {
-                    if (cpt == 0)
-                    {
-                        moveBack = true;
-                        timer = 0;
-                        OpenDoor = true;
-                        cpt++;
-                    }
-                    else
-                    {
-                        if (willBeBack)
-                        {
-                            loupiottes[loupiottesCount].GetComponent<SD_LoupiotteActivated>().activated = false;
-                            loupiottes[loupiottesCount].GetComponent<SpriteRenderer>().color = Color.white;
-                            loupiottes[0].GetComponent<SD_LoupiotteActivated>().activated = true;
-                            loupiottes[0].GetComponent<SpriteRenderer>().color = Color.blue;
-                            
-                        }
-                    }
-                    if (willBeBack)
-                        closeDoor = false;
-                    
-                }
-                
-
-            }
+            Activation();
+            cpt++;
         }
 
-        if (OpenDoor2)
-        {
-          
-            if (timer > timeToReach)
-            {
-                if (transform.GetChild(1).localScale.x >= 0.1f)
-                {
-                    transform.GetChild(1).position -= new Vector3(0.02f, 0, 0);
-                    transform.GetChild(1).localScale -= new Vector3(0.01f, 0, 0);
-
-                }
-                else
-                {
-                    if (cpt == 0)
-                    {                     
-                        closeDoor2 = true;
-                    }
-                    
-                    if (willBeBack)
-                        OpenDoor2 = false;
-
-                }
-            }
-        }
-        if (closeDoor2)
-        {
-           
-            if (timer > timeToReach)
-            {
-                if (transform.GetChild(1).localScale.x <= 1.1f)
-                {
-                    transform.GetChild(1).position += new Vector3(0.02f, 0, 0);
-                    transform.GetChild(1).localScale += new Vector3(0.01f, 0, 0);
-
-                }
-                else
-                {
-                    if (cpt == 0)
-                    {
-                        OpenDoor2 = true;
-                    }
-                   
-                    if (willBeBack)
-                        closeDoor2 = false;
-
-                }
-
-            }
-        }
-        if (moveBack)
-        {
-            camera.transform.position = Vector3.MoveTowards(camera.transform.position,
-                                                           playerCam.transform.GetChild(0).position,
-                                                            0.05f);
-        }
-        if (Mathf.Abs(camera.transform.position.x - playerCam.transform.GetChild(0).position.x) < 0.1f && moveBack)
-        {
-            moveBack = false;
-            Time.timeScale = 1;
-            SD_PlayerMovement.Instance.cantDash = false;
-            SD_PlayerMovement.Instance.cantMove = false;
-            SD_PlayerAttack.Instance.cantAttack = false;
-            SD_PlayerAttack.Instance.cantAim = false;
-            camera.SetActive(false);
-            playerCam.SetActive(true);
-            
-        }
-    }
-    public void Triggered(Collider2D collision)
-    {
-        if (collision.gameObject.layer == 14 && !closeDoor && !OpenDoor && loupiottesCount == loupiottes.Count - 1)
+        if (moveBack && camera.activeInHierarchy)
         {
             Time.timeScale = 0;
+            camera.transform.position = Vector3.MoveTowards(camera.transform.position,
+                                                           playerCam.transform.GetChild(0).position, 0.05f);
+            if (Mathf.Abs(camera.transform.position.x - playerCam.transform.GetChild(0).position.x) < 0.1f && moveBack)
+            {
+                moveBack = false;
+                Time.timeScale = 1;
+                SD_PlayerMovement.Instance.cantDash = false;
+                SD_PlayerMovement.Instance.cantMove = false;
+                SD_PlayerAttack.Instance.cantAttack = false;
+                SD_PlayerAttack.Instance.cantAim = false;
+                camera.SetActive(false);
+                playerCam.SetActive(true);
+                Time.timeScale = 1;
+                timer = 0;
+            }
+
+        }
+
+    }
+    public void MoveBack()
+    {
+        doorAnimator.speed = 1f;
+        moveBack = true;
+    }
+    public void ForOppening()
+    {
+        if (Open)
+        {
+            if (willBeBack)
+            {
+                loupiottes[loupiottesCount].GetComponent<SD_LoupiotteActivated>().activated = false;
+                loupiottes[loupiottesCount].GetComponent<SpriteRenderer>().color = Color.white;
+                loupiottes[0].GetComponent<SD_LoupiotteActivated>().activated = true;
+                loupiottes[0].GetComponent<SpriteRenderer>().color = Color.blue;
+            }
+            closeDoor = false;
+        }
+    }
+    public void ForClosing()
+    {
+        if (!Open)
+        {
+            if (willBeBack)
+            {
+                loupiottes[loupiottesCount].GetComponent<SD_LoupiotteActivated>().activated = false;
+                loupiottes[loupiottesCount].GetComponent<SpriteRenderer>().color = Color.white;
+                loupiottes[0].GetComponent<SD_LoupiotteActivated>().activated = true;
+                loupiottes[0].GetComponent<SpriteRenderer>().color = Color.blue;
+            }
+            closeDoor = false;
+        }
+    }
+
+    public void Triggered(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 14 && !closeDoor  && loupiottesCount == loupiottes.Count - 1)
+        {
+            Time.timeScale = 0;
+            moveBack = false;
             SD_PlayerMovement.Instance.cantDash = true;
             SD_PlayerMovement.Instance.cantMove = true;
             SD_PlayerAttack.Instance.cantAttack = true;
@@ -241,6 +158,8 @@ public class SD_Loupiotte : MonoBehaviour
             move = true;
             cpt = 0;
             loupiottesCount = 0;
+            timer = 1;
+            closeDoor = true;
             foreach (GameObject loupiots in loupiottes)
             {
                 loupiots.GetComponent<SpriteRenderer>().color =Color.white;
--- ZeldaLikeUnity/Assets/Scripts/Environement/SD_Trigger.cs
+++ ZeldaLikeUnity/Assets/Scripts/Environement/SD_Trigger.cs
@@ -15,6 +15,8 @@ public class SD_Trigger : MonoBehaviour
     bool moveBack;
     int cpt;
     GameObject interactButton;
+    Animator doorAnimator;
+    public bool Open;
     private void Start()
     { 
 
@@ -22,6 +24,9 @@ public class SD_Trigger : MonoBehaviour
         interactButton.transform.SetParent(SD_PlayerMovement.Instance.transform);
         interactButton.transform.position = new Vector2(interactButton.transform.parent.position.x,
                                                         interactButton.transform.parent.position.y + 1);
+        doorAnimator = GetComponent<Animator>();
+        doorAnimator.SetBool("Oppen", Open);
+        timer = 1;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -34,6 +39,8 @@ public class SD_Trigger : MonoBehaviour
         if (Input.GetButtonDown("Interact") && !closeDoor)
         {
             Time.timeScale = 0;
+            timer =1;
+            moveBack = false;
             SD_PlayerMovement.Instance.cantDash = true;
             SD_PlayerMovement.Instance.cantMove = true;
             SD_PlayerAttack.Instance.cantAttack = true;
@@ -43,6 +50,7 @@ public class SD_Trigger : MonoBehaviour
             camera.transform.position = new Vector3(SD_PlayerMovement.Instance.transform.position.x, SD_PlayerMovement.Instance.transform.position.y, -10);
             move = true;
             cpt = 0;
+            closeDoor = true;
             interactButton.SetActive(false);
         }
     }
@@ -50,103 +58,73 @@ public class SD_Trigger : MonoBehaviour
     {
         interactButton.SetActive(false);
     }
+    void Activation()
+    {
+        doorAnimator.SetTrigger("Activated");
+        doorAnimator.speed = 10f;
+    }
+    IEnumerator WaitToComeBack()
+    {
+        timer = 1;
+        yield return new WaitForSeconds(timeToReach);
+        doorAnimator.SetTrigger("ComeBack");
+
+
+    }
     private void Update()
     {
         if (move)
             camera.transform.position = Vector3.MoveTowards(camera.transform.position,
                                                             new Vector3(transform.GetChild(0).position.x, transform.GetChild(0).position.y, -10),
                                                             0.05f);
-        if (Mathf.Abs(camera.transform.position.x - transform.GetChild(0).position.x) < 0.1f)
+        if (timer == 0)
         {
+            StartCoroutine(WaitToComeBack());
+
+        }
+        if (Mathf.Abs(camera.transform.position.x - transform.GetChild(0).position.x) < 0.1f && cpt == 0)
+        {
+            Time.timeScale = 0.1f;
             move = false;
-            if (transform.GetChild(0).localScale.x >= 0.1f)
-            {
-                OpenDoor = true;
-
-            }
-            else
-            {
-                closeDoor = true;
-            }
-
-
+            Activation();
+            cpt++;
         }
-        if (OpenDoor)
+
+        if (moveBack && camera.activeInHierarchy)
         {
-            if (cpt == 0)
-                timer = timeToReach;
-            timer += 0.01f;
-            if (timer > timeToReach)
-            {
-                if (transform.GetChild(0).localScale.x >= 0.1f)
-                {
-                    transform.GetChild(0).position -= new Vector3(0.02f, 0, 0);
-                    transform.GetChild(0).localScale -= new Vector3(0.01f, 0, 0);
-
-                }
-                else
-                {
-                    if (cpt == 0)
-                    {
-                        moveBack = true;
-                        timer = 0;
-                        closeDoor = true;
-                        cpt++;
-                    }
-                    OpenDoor = false;
-
-                }
-                if (transform.GetChild(0).localScale.x >= 0.0909f && !OpenDoor || transform.GetChild(0).localScale.x <= 0.101f && !OpenDoor)
-                {
-
-                    transform.GetChild(0).localScale = new Vector3(0.05f, 1, 0);
-                }
-            }
-        }
-        if (closeDoor)
-        {
-            if (cpt == 0)
-                timer = timeToReach;
-            timer += 0.01f;
-            if (timer > timeToReach)
-            {
-                if (transform.GetChild(0).localScale.x <= 1f)
-                {
-                    transform.GetChild(0).position += new Vector3(0.02f, 0, 0);
-                    transform.GetChild(0).localScale += new Vector3(0.01f, 0, 0);
-
-                }
-                else
-                {
-                    if (cpt == 0)
-                    {
-                        moveBack = true;
-                        timer = 0;
-                        OpenDoor = true;
-                        cpt++;
-                    }
-                    closeDoor = false;
-
-                }
-
-            }
-        }
-        if (moveBack)
-        {
+            Time.timeScale = 0;
             camera.transform.position = Vector3.MoveTowards(camera.transform.position,
-                                                           playerCam.transform.GetChild(0).position,
-                                                            0.05f);
+                                                           playerCam.transform.GetChild(0).position, 0.05f);
+            if (Mathf.Abs(camera.transform.position.x - playerCam.transform.GetChild(0).position.x) < 0.1f && moveBack)
+            {
+                moveBack = false;
+                Time.timeScale = 1;
+                SD_PlayerMovement.Instance.cantDash = false;
+                SD_PlayerMovement.Instance.cantMove = false;
+                SD_PlayerAttack.Instance.cantAttack = false;
+                SD_PlayerAttack.Instance.cantAim = false;
+                camera.SetActive(false);
+                playerCam.SetActive(true);
+                Time.timeScale = 1;
+                timer = 0;
+            }
+
         }
-        if (Mathf.Abs(camera.transform.position.x - playerCam.transform.GetChild(0).position.x) < 0.1f && moveBack)
-        {
-            moveBack = false;
-            Time.timeScale = 1;
-            SD_PlayerMovement.Instance.cantDash = false;
-            SD_PlayerMovement.Instance.cantMove = false;
-            SD_PlayerAttack.Instance.cantAttack = false;
-            SD_PlayerAttack.Instance.cantAim = false;
-            camera.SetActive(false);
-            playerCam.SetActive(true);
-        }
+
+    }
+    public void MoveBack()
+    {
+        doorAnimator.speed = 1f;
+        moveBack = true;
+    }
+    public void ForOppening()
+    {
+        if (Open)
+            closeDoor = false;
+    }
+    public void ForClosing()
+    {
+        if (!Open)
+            closeDoor = false;
     }
 }
