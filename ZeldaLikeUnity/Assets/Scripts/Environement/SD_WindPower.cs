64f58ca3f083542e2a1f237d4d413317ab6e1608
Dubois <samuel.m.dubois@gmail.com>
2020-04-30 17:04:55 +0200

    Statue animation

--- ZeldaLikeUnity/Assets/Scripts/Environement/SD_WindPower.cs
+++ ZeldaLikeUnity/Assets/Scripts/Environement/SD_WindPower.cs
@@ -2,10 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Player;
+using Management;
 
 public class SD_WindPower : MonoBehaviour
 {
+    public GameObject end;
     public GameObject interactButton;
+    public GameObject text;
+    bool canText;
+    private void Update()
+    {
+        if (!text.activeSelf && canText)
+        {
+            canText = false;
+            SD_PlayerAttack.Instance.hasWind = true;
+            SD_PlayerAttack.Instance.cantAttack = true;
+            SD_PlayerAttack.Instance.cantAim = true;
+            SD_PlayerMovement.Instance.cantMove = true;
+            SD_PlayerMovement.Instance.cantDash = true;
+            GetComponent<Animator>().enabled = true;
+            end.transform.position = SD_PlayerMovement.Instance.transform.position;
+            //StartCoroutine( GameManagerV2.Instance.GamePadeShake(0.2f, 4.75f));
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         interactButton.SetActive(true);
@@ -18,10 +37,10 @@ public class SD_WindPower : MonoBehaviour
                                                         interactButton.transform.parent.position.y + 1);
         if (collision.gameObject.tag == "Player"  && Input.GetButtonDown("Interact"))
         {
-            SD_PlayerAttack.Instance.hasWind = true;
-            GetComponent<Animator>().enabled = true;
-
+            canText = true;
             interactButton.SetActive(false);
+            text.SetActive(true);
+            StartCoroutine( text.GetComponentInChildren<SD_TextTest>().Text());
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
@@ -35,6 +54,14 @@ public class SD_WindPower : MonoBehaviour
     }
     public void StopANimation()
     {
+        SD_PlayerAttack.Instance.cantAim = false;
+        SD_PlayerAttack.Instance.cantAttack = false;
+        SD_PlayerMovement.Instance.cantMove = false;
+        SD_PlayerMovement.Instance.cantDash = false;
         GetComponent<Animator>().enabled = false;
     }
+
+    
+
+
 }
