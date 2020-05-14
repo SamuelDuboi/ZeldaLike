4b6161ad0195c0ede1d2836d6a68e7c4d91a75ce
Dubois <samuel.m.dubois@gmail.com>
2020-05-14 11:43:18 +0200

    Fix boss 1

---
+++ ZeldaLikeUnity/Assets/Scripts/Boss/LaserLuncher.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserLuncher : MonoBehaviour
+{
+    public GameObject laser;
+    public void Shoot()
+    {
+        Instantiate(laser, transform.position, Quaternion.identity);
+    }
+    public void Destroy()
+    {
+        Destroy(gameObject);
+    }
+}
