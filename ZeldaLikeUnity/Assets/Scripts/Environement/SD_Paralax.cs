a84b8a3cd58566e00a79a8893c89919ec4943735
Dubois <samuel.m.dubois@gmail.com>
2020-05-28 16:11:27 +0200

    arrow + bug parrallax via chapitres + ui focus avec souris

--- ZeldaLikeUnity/Assets/Scripts/Environement/SD_Paralax.cs
+++ ZeldaLikeUnity/Assets/Scripts/Environement/SD_Paralax.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 
 public class SD_Paralax : MonoBehaviour
 {
-    public GameObject camera;
+    public GameObject cameraMain;
     [Range(0,1)]
     public float paralaxSpeed;
     Vector2 lasteCamera;
     // Start is called before the first frame update
     void Start()
     {
-        lasteCamera = camera.transform.position;
+        lasteCamera = cameraMain.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 deltaPosition = new Vector3(camera.transform.position.x - lasteCamera.x, camera.transform.position.y - lasteCamera.y,0);
+        Vector3 deltaPosition = new Vector3(cameraMain.transform.position.x - lasteCamera.x, cameraMain.transform.position.y - lasteCamera.y,0);
         transform.position += Vector3.right* (deltaPosition.x * paralaxSpeed);
         transform.position += Vector3.up* (deltaPosition.y * paralaxSpeed);
-        lasteCamera = camera.transform.position;
+        lasteCamera = cameraMain.transform.position;
         
     }
 }
---
+++ ZeldaLikeUnity/Assets/Scripts/Managers/UIFocus.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIFocus : MonoBehaviour
+{
+    GameObject lastselect;
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        lastselect = new GameObject();
+    }
+    void Update()
+    {
+        if (EventSystem.current != null)
+        {
+            if (EventSystem.current.currentSelectedGameObject == null)
+            {
+                EventSystem.current.SetSelectedGameObject(lastselect);
+            }
+            else if (lastselect != EventSystem.current.currentSelectedGameObject)
+            {
+                lastselect = EventSystem.current.currentSelectedGameObject;
+            }
+        }
+    }
+}
